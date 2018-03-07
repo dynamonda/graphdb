b681d3bc9fcf91330341032ffcbc55871140711a
dynamonda <v7gj9kk@gmail.com>
2018-03-07 11:31:09 +0900

    Add menu in electron

--- electron/main.js
+++ electron/main.js
@@ -3,6 +3,7 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
+var Menu = electron.Menu;
 
 var mainWindow = null;
 
@@ -13,6 +14,8 @@ app.on('window-all-closed', function(){
 });
 
 app.on('ready', function(){
+    Menu.setApplicationMenu(menu);
+
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
@@ -20,3 +23,45 @@ app.on('ready', function(){
         mainWindow = null;
     });
 });
+
+var MenuTemplate = [
+    {
+        label: 'ReadUs',
+        submenu: [
+            {label: `Quit`, accelerator: 'Command+Q', click: function(){app.quit()}}
+        ]
+    }, {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Open', 
+                accelerator: 'Command+O', 
+                // [FixMe]: dialogモジュールエラー
+                click: function(){
+                    require('dialog').showOpenDialog( {properties:['openDirectory']},
+                        function(baseDir){
+                            if(baseDir && baseDir[0]){
+                                openWindow(baseDir[0]);
+                            }
+                        }
+                    );
+                }
+            }
+        ]
+    }, {
+        label: 'View',
+        submenu: [
+            { 
+                label: 'Reload',
+                accelerator: 'Command+R', 
+                click: function(){ BrowserWindow.getFocusedWindow().reload(); }
+            }, {
+                label: 'Toggle DevTools',
+                accelerator: 'Alt+Command+I',
+                click: function(){ BrowserWindow.getFocusedWindow().toggleDevTools(); }
+            }
+        ]
+    }
+];
+
+var menu = Menu.buildFromTemplate(MenuTemplate);
